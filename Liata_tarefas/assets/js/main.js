7938dc941ea49a2c1476a75c38226103526508df
iago bruno <iago.silva6969@gmail.com>
2025-06-29 19:04:41 -0300

    +

--- Liata_tarefas/assets/js/main.js
+++ Liata_tarefas/assets/js/main.js
@@ -7,22 +7,64 @@ btnTarefa.addEventListener('click', (e) => {
     if(!inputTarefa.value) return;
     console.log(inputTarefa.value);
     criarLi(inputTarefa.value);
-    inputTarefa.value = '';
+    limparInput();
 });
+
+function limparInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
 
 inputTarefa.addEventListener('keypress', (e) => {
     if(e.keyCode === 13) {
         if(!inputTarefa.value) return;
         console.log(inputTarefa.value);
         criarLi(inputTarefa.value);
-        inputTarefa.value = '';
+        limparInput();
     }
 });
 
 function criarLi(tarefa) {
     listaTarefas.innerHTML += `
         <li>
-            ${tarefa} <button type="button">Apagar</button>
+            ${tarefa} <button class="apagar" type="button">Apagar</button>
         </li>
     `;
+    salvarTarefas()
 }
+
+document.addEventListener('click', (e) => {
+    const el = e.target;
+
+    if(el.classList.contains('apagar')) {
+        //console.log(el.parentElement);
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = listaTarefas.querySelectorAll('li');
+    const tarefas = [];
+
+    for(let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        tarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(tarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+    
+}
+
+function carregarTarefas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const lista = JSON.parse(tarefas);
+
+    for(let tarefa of lista) {
+        criarLi(tarefa);
+    }
+}
+
+carregarTarefas()
