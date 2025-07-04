f77cb26f2772bbb3e41a4fdd2224c1faa026ffc4
iago bruno <iago.silva6969@gmail.com>
2025-07-04 17:07:41 -0300

    +

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
-(function() {
-    console.log('Hello, World!');
-})();
+
+// pessoa = {
+//     nome: "João",
+//     idade: 30,
+//     profissao: "Engenheiro"
+// };
+
+pessoa = (nome, idade, peso) => {
+    nome = nome;
+    idade = idade;
+    peso = peso;
+    falar = () => {
+        console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos e peso ${peso} kg.`);
+    }
+}
+
+pessoa.falar();
+
+
