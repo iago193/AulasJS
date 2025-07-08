07b01daf9ce8ad3331f9c7c032e633179f5454a8
iago bruno <iago.silva6969@gmail.com>
2025-07-08 19:01:58 -0300

    +

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 
-// Factory functions
-class Produto extends Desconsto {
-    
+function rand(max, min) {
+    min *= 1000;
+    max *= 1000;
+
+    return Math.floor(Math.random() * max - min + min);
 }
 
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+   
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+
+    });
+}
+
+esperaAi('Frase 1', rand(1, 3))
+    .then(reposta => {
+        console.log(reposta);
+        return esperaAi('Frase 2', rand(1, 3));
+    })
+    .then(reposta => {
+        console.log(reposta);
+        return esperaAi('Frase 3', rand(1, 3));
+    })
+    .then( reposta => {
+        console.log(reposta);
+    })
+    .catch();
 
 
 
