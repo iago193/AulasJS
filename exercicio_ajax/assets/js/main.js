f2148b55317d64dd0c3f6b7fb000d1249e56a6b9
iago bruno <iago.silva6969@gmail.com>
2025-07-09 18:52:47 -0300

    +

---
+++ exercicio_ajax/assets/js/main.js
@@ -0,0 +1,58 @@
+const request = (obj) => {
+
+    return new Promise((resolve, reject) => {
+
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+
+        xhr.addEventListener('load', () => {
+
+            if (xhr.status >= 200 && xhr.status <= 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        });
+
+    });
+
+};
+
+document.addEventListener('click', (e) => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    const herf = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: herf,
+    }
+
+
+    // request(objConfig).then((response) => {
+    //     carregaResultado(response);
+    // }).catch((e) => {
+    //     console.log(e);
+    // });
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = response;
+}
