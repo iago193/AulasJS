3cfd2b5fcb3a02af5ac4ed88b219b61dd2b15467
iago bruno <iago.silva6969@gmail.com>
2025-07-10 19:27:21 -0300

    +

--- exercicio_fetch_JSON/assets/js/main.js
+++ exercicio_fetch_JSON/assets/js/main.js
@@ -1,27 +1,52 @@
-fetch('pessoas.json')
-  .then(resposta => resposta.json())
-  .then(json => carregaElementosNaPagina(json));
+// fetch('pessoas.json')
+//   .then((resposta) => {
+//     return resposta.json();
+//   })
+//   .then((json) => {
+//     carregaElementosNaPagina(json);
+//   });
 
-// axios('pessoas.json')
-//   .then(resposta => carregaElementosNaPagina(resposta.data));
+  axios('pessoas.json')
+  .then((json) => {
+    carregaElementosNaPagina(json.data);
+  });
 
-function carregaElementosNaPagina(json) {
-  const table = document.createElement('table');
 
-  for(let pessoa of json) {
-    const tr = document.createElement('tr');
 
-    let td1 = document.createElement('td');
-    td1.innerHTML = pessoa.nome;
-    tr.appendChild(td1);
+  function carregaElementosNaPagina(json) {
+    const table = document.createElement('table');
+    for(let pessoa of json) {
+      // const tr = document.createElement('tr');
 
-    let td2 = document.createElement('td');
-    td2.innerHTML = pessoa.idade;
-    tr.appendChild(td2);
+      // let td = document.createElement('td');
+      // td.innerHTML = pessoa.nome;
+      // tr.appendChild(td);
 
-    table.appendChild(tr);
+      // td = document.createElement('td');
+      // td.innerHTML = pessoa.salario;
+      // tr.appendChild(td);
+
+      // td = document.createElement('td');
+      // td.innerHTML = pessoa.idade;
+      // tr.appendChild(td);
+
+      // table.appendChild(tr);
+
+      const resultado = document.querySelector('.resultado');
+      //resultado.appendChild(table);
+
+      resultado.innerHTML += `
+      <p>
+        <strong>Nome:</strong> ${pessoa.nome} <br/>
+        <strong>CPF:</strong> ${pessoa.cpf} <br/>
+        <strong>Email:</strong> ${pessoa.email} <br/>
+        <strong>Empresa:</strong> ${pessoa.empresa} <br/>
+        <strong>Estado:</strong> ${pessoa.estado} <br/>
+        <strong>Idade:</strong> ${pessoa.idade} <br/>
+        <strong>Salario:</strong> ${pessoa.salario} <br/>
+        <strong>Sexo:</strong> ${pessoa.sexo === '' ? 'não definido' : pessoa.sexo}
+      <p/>
+
+      <hr/>`;
+    }
   }
-
-  const resultado = document.querySelector('.resultado');
-  resultado.appendChild(table);
-}
